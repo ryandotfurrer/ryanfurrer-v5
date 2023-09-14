8a8117e8b933864791cc879bb1f1b2acf0db6b3b
Ryan Furrer <ryandotfurrer@gmail.com>
2023-09-14 11:29:20 -0400

    Initial commit with Storyblok integrated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,29 @@
 import { defineConfig } from 'astro/config';
-
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: 
+  [
+    react(), 
+    tailwind(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        // Add your components here
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: 'us', // optional,  or 'eu' (default)
+      },
+    }),
+  ]
 });
