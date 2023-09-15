90f9243d0c6884fe5c464bb9957454ab26c2bb49
Ryan Furrer <ryandotfurrer@gmail.com>
2023-09-15 11:54:39 -0400

    Added prettier config.

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: 
+  [
+    'prettier-plugin-astro',
+    'prettier-plugin-tailwindcss'
+  ],
+  tailwindFunctions: ['tw'],
+};
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import '../styles/global.css'
 export default function Hero() {
     return(
         <section className='min-h-[65dvh] bg-zinc-100 dark:bg-zinc-800 '>
-            <div className="min-h-[65dvh] flex flex-col justify-center container px-4 mx-auto">
+            <div className=" flex flex-col justify-center container px-4 mx-auto min-h-[65dvh]">
                 <h1>Page Title</h1>
                 <h3>Page Description</h3>
             </div>
