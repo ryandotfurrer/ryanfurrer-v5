1b333d6cc33216eb96e0691cdccad023d4b2ae2a
Ryan Furrer <ryandotfurrer@gmail.com>
2023-09-16 19:45:14 -0400

    Refined color palette
    1. gray --> zinc
      - zinc is a less saturated cool gray than gray.
    2. Made footer border-top responsive to theme changes.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import FooterNavBar from "./FooterNavbar";
 
 export default function Footer() {
   return (
-    <footer className="h-min-full mx-auto w-full border-t-2 border-gray-800 border-opacity-100 pb-8 pt-20">
+    <footer className="h-min-full mx-auto w-full border-t-2 border-zinc-200 border-opacity-100 pb-8 pt-20 dark:border-zinc-800">
       <div
         id="footer-inside-container"
         className="container mx-auto grid gap-4 px-8"
