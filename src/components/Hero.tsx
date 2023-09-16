18ab756b5eb7697b5874c183037e3b864276fe6b
Ryan Furrer <ryandotfurrer@gmail.com>
2023-09-16 15:56:12 -0400

    Refined styles of Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,17 +2,24 @@ import "../styles/global.css";
 
 export default function Hero() {
   return (
-    <section id="hero" className="mx-auto max-h-[100dvh] min-h-[75vh]">
-      <div className="container mx-auto grid max-h-[100dvh] min-h-[75dvh] grid-cols-1 justify-around px-8 lg:grid-cols-2 lg:place-items-center">
-        <div id="hero-img-container" className="w-full">
-          <img src="/favicon.svg" alt="logo" />
-        </div>
-        <div id="hero-text-container" className="grid place-content-start">
-          <h2>These are some words</h2>
-          <h3 className="opacity-75">
-            And these are some other words, can't you see?
-          </h3>
-        </div>
+    <section
+      id="hero"
+      className="container mx-auto grid h-3/4 grid-cols-1 lg:grid-cols-2 lg:place-items-center"
+    >
+      <div
+        id="hero-img-container"
+        className="container self-center lg:self-auto"
+      >
+        <img src="/favicon.svg" alt="logo" />
+      </div>
+      <div
+        id="hero-text-container"
+        className="align-end container self-end text-center lg:self-auto"
+      >
+        <h2>These are some words</h2>
+        <h3 className="opacity-75">
+          And these are some other words, can't you see?
+        </h3>
       </div>
     </section>
   );
