a92e5bbae74ed5412fc5959f9d698b871c3250fc
Ryan Furrer <ryandotfurrer@gmail.com>
2023-09-16 11:10:09 -0400

    fix random component text in /blog/[...slub].astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 // import { loadEnv } from 'vite';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -13,28 +13,25 @@ export default defineConfig({
     plugins: [basicSsl()],
     server: {
       https: true,
-    }
+    },
   },
-  integrations: 
-  [
-    react(), 
-    tailwind(
-      {
-        applyBaseStyles: false,
-      }
-    ),
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     storyblok({
       accessToken: "OvG6RltzzmQpZ8WXKtSeEwtt",
-      apiOptions: {      
+      apiOptions: {
         // Choose your Storyblok space region
-        region: 'us', // optional,  or 'eu' (default)
+        region: "us", // optional,  or 'eu' (default)
       },
       components: {
         // Add your components here
-        blogPost: 'storyblok/BlogPost',
-        blogPostList: 'storyblok/BlogPostList',
-        page: 'storyblok/Page'
+        blogPost: "storyblok/BlogPost",
+        blogPostList: "storyblok/BlogPostList",
+        page: "storyblok/Page",
       },
     }),
-  ]
+  ],
 });
