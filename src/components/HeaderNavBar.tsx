aec732ab4aa6f7a33d39b0f1784b3edc72757b0a
Ryan Furrer <ryandotfurrer@gmail.com>
2023-09-16 15:35:09 -0400

    Improved HeaderNavBar styles.

--- src/components/HeaderNavBar.tsx
+++ src/components/HeaderNavBar.tsx
@@ -2,18 +2,15 @@ import "../styles/global.css";
 
 export default function HeaderNavBar() {
   return (
-    <nav className="container mx-auto flex px-8 py-12 text-lg font-semibold">
-      <a
-        href="/"
-        className="mr-auto text-3xl font-bold opacity-100 hover:text-emerald-300"
-      >
-        Ryan Furrer
-      </a>
-      <ul className="flex gap-4">
+    <nav id="header-nav">
+      <h1 className="mr-auto text-3xl opacity-100 hover:text-emerald-300 md:text-4xl  ">
+        <a href="/" className="hover:no-underline">
+          Ryan Furrer
+        </a>
+      </h1>
+      <ul className="flex gap-[1em]">
         <li>
-          <a href="/" className="text-current/50">
-            Home
-          </a>
+          <a href="/">Home</a>
         </li>
         <li>
           <a href="/blog">Blog</a>
