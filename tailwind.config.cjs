c40053ad242d61630e0381af4e8ade286d9e8c39
Ryan Furrer <ryandotfurrer@gmail.com>
2024-01-16 14:39:36 -0500

    Remove any unnecessary imports of /src/styles/global.css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,7 +13,7 @@ module.exports = {
     fontSize: {
       xs: "clamp(0.7813rem, 0.7747rem + 0.0326vw, 0.8rem)",
       sm: "clamp(0.9375rem, 0.9158rem + 0.1087vw, 1rem)",
-      base: "clamp(1.125rem, 1.0815rem + 0.2174vw, 1.25rem)",
+      base: "clamp(1rem, 1.0815rem + 0.2174vw, 1.25rem)",
       lg: "clamp(1.35rem, 1.2761rem + 0.3696vw, 1.5625rem)",
       xl: "clamp(1.62rem, 1.5041rem + 0.5793vw, 1.9531rem)",
       "2xl": "clamp(1.9438rem, 1.7707rem + 0.8652vw, 2.4413rem)",
