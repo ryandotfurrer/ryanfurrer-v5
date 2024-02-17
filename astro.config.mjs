f2728a8692ae67bbec02d54b6b91df39bd2fa5af
Ryan Furrer <ryandotfurrer@gmail.com>
2024-02-17 14:15:19 -0500

    Reconfigure storyblok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,48 +1,96 @@
 import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import react from "@astrojs/react";
-import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
-import netlify from "@astrojs/netlify";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.ryanfurrer.com",
   prefetch: {
     prefetchAll: true,
-    defaultStrategy: "hover"
+    defaultStrategy: "hover",
   },
   markdown: {
     shikiConfig: {
       theme: "github-dark",
       wrap: true,
-      langs: ["astro", "css", "docker", "dockerfile", "elixer", "gql", "graphql", "html", "javascript", "js", "json", "jsx", "less", "liquid", "lua", "markdown", "md", "nginx", "php", "postcss", "powershell", "ps", "py", "python", "rb", "rs", "ruby", "rust", "sass", "scss", "sql", "svelte", "swift", "toml", "ts", "tsx", "typescript", "vue-html", "vue", "vue", "yaml", "yml"]
-    }
+      langs: [
+        "astro",
+        "css",
+        "docker",
+        "dockerfile",
+        "elixer",
+        "gql",
+        "graphql",
+        "html",
+        "javascript",
+        "js",
+        "json",
+        "jsx",
+        "less",
+        "liquid",
+        "lua",
+        "markdown",
+        "md",
+        "nginx",
+        "php",
+        "postcss",
+        "powershell",
+        "ps",
+        "py",
+        "python",
+        "rb",
+        "rs",
+        "ruby",
+        "rust",
+        "sass",
+        "scss",
+        "sql",
+        "svelte",
+        "swift",
+        "toml",
+        "ts",
+        "tsx",
+        "typescript",
+        "vue-html",
+        "vue",
+        "vue",
+        "yaml",
+        "yml",
+      ],
+    },
   },
   vite: {
     plugins: [basicSsl()],
     server: {
       open: "/src/pages/index.astro",
-      https: true
-    }
-  },
-  integrations: [react(), tailwind({
-    applyBaseStyles: false
-  }), storyblok({
-    accessToken: "OvG6RltzzmQpZ8WXKtSeEwtt",
-    apiOptions: {
-      // Choose your Storyblok space region
-      region: "us" // optional,  or 'eu' (default)
+      https: true,
     },
-    components: {
-      // Add your components here
-      blogPost: "storyblok/BlogPost",
-      blogPostList: "storyblok/BlogPostList",
-      page: "storyblok/Page"
-    }
-  }), icon()],
-  output: "server",
-  adapter: netlify()
+  },
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    storyblok({
+      accessToken: "OvG6RltzzmQpZ8WXKtSeEwtt",
+      components: {
+        // Add your components here
+        blogPost: "storyblok/BlogPost",
+        blogPostList: "storyblok/BlogPostList",
+        blogPostListFeatured: "storyblok/BlogPostListFeatured",
+        page: "storyblok/Page",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        region: "us", // optional,  or 'eu' (default)
+      },
+    }),
+    icon(),
+  ],
 });
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+STORYBLOK_TOKEN = YOUR_PREVIEW_TOKEN = OvG6RltzzmQpZ8WXKtSeEwtt;
